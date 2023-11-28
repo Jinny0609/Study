25c33286269d753630b8e7452ca4aaf370710521
Jinny0609 <wlsghwns1@naver.com>
2023-11-28 23:13:09 +0900

    한번에 삽입

--- MongooseBasics/index.js
+++ MongooseBasics/index.js
@@ -9,19 +9,24 @@ mongoose.connect('mongodb://localhost:27017/test')
     console.error(err);
   });
 
-  // {
-  //   title: 'Amadeus',
-  //   year: 1986,
-  //   score: 9.2,
-  //   rating: 'R'
-  // }
-
   const movieSchema = new mongoose.Schema({
     title: String,
     year: Number,
     score: Number,
     rating: String
   });
-
+  
 const Movie = mongoose.model('Movie', movieSchema);
 const Amadeus = new Movie({ title : 'Amadeus', year: 1986, score: 9.2, rating: 'R'})
+
+Movie.insertMany([
+  { title: "Amelie", releaseYear: 2022, rating: 8.3, grade: "R" },
+  { title: "Alien", releaseYear: 1979, rating: 8.3, grade: "R" },
+  { title: "The Iron Giant", releaseYear: 1999, rating: 8.3, grade: "PG" },
+  { title: "Stand By Me", releaseYear: 1986, rating: 8.3, grade: "R" },
+  { title: "Moonrise Kingdom", releaseYear: 2012, rating: 8.3, grade: "PG-13" },
+])
+  .then(data=> {
+    console.log("IT WORKED!")
+    console.log(data);
+  })

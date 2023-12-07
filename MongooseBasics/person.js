73656687dc2607a1d35ef6cffd96789298dc672a
Jinny0609 <wlsghwns1@naver.com>
2023-12-07 16:39:21 +0900

    Mongoose를 미들웨어로 정의하기 12.07

--- MongooseBasics/person.js
+++ MongooseBasics/person.js
@@ -17,6 +17,16 @@ personSchema.virtual('fullName').get(function() {
     return `${this.first} ${this.last}`;
 });
 
+personSchema.pre('save', async function () {
+  this.first = 'Yo';
+  this.last = 'MAMA';
+  console.log("ABOUT TO SAVE!!!!");
+})
+
+personSchema.post('save', async function () {
+  console.log("JUST SAVED!!!!");
+})
+
 const Person = mongoose.model('Person', personSchema);
 
 // const tammy = new Person({ first: 'Tammy', last: 'Chow' }); 

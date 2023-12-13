a820d15af5f9673d5ccfd59a7ed08c490b2ee4fe
Jinny0609 <wlsghwns1@naver.com>
2023-12-13 10:13:00 +0900

    프로덕트 만들기 12.13

--- Mongoose_Express/index.js
+++ Mongoose_Express/index.js
@@ -18,12 +18,26 @@ mongoose.connect('mongodb://localhost:27017/farmStand')
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true})) //추가
 
 app.get('/products', async (req, res) => {
     const products = await Product.find({})
     console.log(products)
     res.render('products/index', { products });
 });
+
+app.get('/products/new', (req, res) => {
+  res.render('products/new');
+});
+
+app.post('/products', async (req, res) => {
+  // console.log(req.body) // 생성된 상품 정보를 콘솔에 출력
+  const newProduct = new Product(req.body); // 새 상품 만들기
+  await newProduct.save(); 
+  console.log(newProduct);  // 작업을 알기위해서 콘솔에 출력
+  // res.send('making your product!')
+  res.redirect(`/products/${newProduct._id}`)
+})
 
 app.get('/products/:id', async (req, res) => {
   const { id } = req.params;

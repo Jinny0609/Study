58f97cc80a0d2065699c8b1c55a421a6d5e2edaa
Jinny0609 <wlsghwns1@naver.com>
2023-12-20 20:08:17 +0900

    캠프그라운드 모델 기초 12.20

--- YelpCamp/app.js
+++ YelpCamp/app.js
@@ -1,6 +1,20 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+const Campground = require('./models/campground');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('데이터베이스 연결됨');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'))
@@ -8,6 +22,18 @@ app.set('views', path.join(__dirname, 'views'))
 app.get('/', (req,res)=> {
     res.render('home')
 }) 
+
+// 라우트 추가
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({
+      name: '나의 뒷마당',
+      price: '50,000원',
+      description: '값 싼 캠핑!',
+      location: '서울',
+    });
+    await camp.save();
+    res.send(camp);
+  });
 
 app.listen(3000, () => {
     console.log('Sercing on port 3000')
